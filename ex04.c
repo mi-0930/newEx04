f66f9e179503c3b1e9f1c15cac978293bebb13cc
mi-0930 <134513668+mi-0930@users.noreply.github.com>
2024-06-28 13:02:40 +0900

    Dice game: initial revision

---
+++ ex04.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+    int a,b,total=0;
+    char name[20];
+    
+    printf("Rolling dice...\n");
+    srand(time(NULL));
+    a=1+rand()%6;
+    b=1+rand()%6;
+    total=a+b;
+    printf("Die 1: %d\n",a);
+    printf("Die 2: %d\n",b);
+    printf("Total value: %d",total);
+    
+    return 0;
+}
